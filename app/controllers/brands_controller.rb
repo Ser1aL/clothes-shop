35e1056a3df570ab013d00cad5ec0fed9ec0fd00
Max Reznichenko <max.reznichenko@gmail.com>
2012-05-22 15:28:48 +0300

    single product page

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -2,9 +2,9 @@ class BrandsController < ApplicationController
   def index
     @brands = begin
       if params[:letter]
-        Brand.find_by_letter(params[:letter])
+        Brand.find_by_letter(params[:letter]).order(:name)
       elsif params[:category_id]
-        Brand.find_by_category_id(params[:category_id])
+        Brand.find_by_category_id(params[:category_id]).order(:name)
       else
         Brand.order(:name).all.group_by{ |brand| brand.name.downcase.first }
       end
