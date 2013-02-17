0beba4b0a310d6562f82fb82156bd11089cc0ea2
Max Reznichenko <max.reznichenko@gmail.com>
2013-02-17 11:01:58 +0200

    small seo changes

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -4,9 +4,11 @@ class ErrorsController < ApplicationController
   def not_found
     @categories = Category.all.group_by(&:top_category).delete_if{ |key, _| key.blank? }
     @countings = CategoryCounting.grouped_by_category
+    render :status => 404
   end
 
   def internal_server_error
+    render :status => 500
   end
 
 end
