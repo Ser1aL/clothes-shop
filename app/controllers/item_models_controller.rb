d19ff6559d1f2f4457ffbb28b884066dd1a1ca05
Max Reznichenko <max.reznichenko@gmail.com>
2013-09-23 16:41:02 +0300

    familiars fix

--- app/controllers/item_models_controller.rb
+++ app/controllers/item_models_controller.rb
@@ -9,7 +9,7 @@ class ItemModelsController < ApplicationController
     @product = ItemModel.find(params[:id])
     @style = @product.product.styles.find(params[:style_id])
     familiars_total_count = @product.category.item_models.count
-    @familiars = ItemModel.where('id != ? and category_id = ?', @product.id, @product.category.id).limit(3).offset(rand(familiars_total_count - 3))
+    @familiars = ItemModel.where('item_models.id != ? and category_id = ?', @product.id, @product.category.id).joins(:product => :styles).where('styles.hidden = 0').limit(3).offset(rand(familiars_total_count - 3))
   end
 
   def preload
